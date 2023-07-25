2804ba12ea5d47072818f1b3539425ce6a968735
Aditha1603 <134356598+Aditha1603@users.noreply.github.com>
2023-07-25 09:26:44 +0530

    Add files via upload

---
+++ 11th  july lab section/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Employee
+{
+    private int empID;
+    private string empName;
+    private string empDesignation;
+
+    // Getter and Setter for empID
+    public int EmpID
+    {
+        get { return empID; }
+        set { empID = value; }
+    }
+
+    // Getter and Setter for empName
+    public string EmpName
+    {
+        get { return empName; }
+        set { empName = value; }
+    }
+
+    // Getter and Setter for empDesignation
+    public string EmpDesignation
+    {
+        get { return empDesignation; }
+        set { empDesignation = value; }
+    }
+}
